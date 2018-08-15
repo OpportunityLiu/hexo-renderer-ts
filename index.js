279b8c2f865971fb8631e2d6a9401cbe724c2bb5
OpportunityLiu <opportunity@live.in>
2018-08-15 16:10:48 +0800

    better hint

--- index.js
+++ index.js
@@ -3,6 +3,23 @@ const didYouMean = require('didyoumean');
 
 /**
  * 
+ * @param {any} map 
+ * @returns {{[k:string]: number}}
+ */
+function getMap(map)
+{
+  let r = {};
+  for (const key in map)
+  {
+    const val = map[key];
+    if (typeof val === 'number')
+      r[key] = val;
+  }
+  return r;
+}
+
+/**
+ * 
  * @param {{text:string, path:string}} data 
  * @param {ts.CompilerOptions} options
  */
@@ -20,12 +37,12 @@ function tsRenderer(data, options)
   var option = { ...fileOptions, ...options };
 
   /**
-   * 
    * @param {string} name 
-   * @param {{[key: string]:number}} map 
+   * @param {{[k:string]: number}} map
    */
   function setEnum(name, map)
   {
+    map = getMap(map);
     var value = option[name];
     if (value === undefined || value === null)
     {
@@ -33,61 +50,44 @@ function tsRenderer(data, options)
       return;
     }
     const valueStr = String(value).trim().toLowerCase();
-    const keys = Object.getOwnPropertyNames(map);
+    const myMap = {};
     for (const key in map)
     {
       const val = map[key];
-      map[map[key]] = val;
-      map[key.toLowerCase()] = val;
+      myMap[key.toLowerCase()] = val;
+      myMap[map[key]] = val;
     }
-    var v = map[valueStr];
+    var v = myMap[valueStr];
     if (v === undefined)
     {
-      const match = didYouMean(valueStr, Object.getOwnPropertyNames(map));
+      const keys = Object.getOwnPropertyNames(map);
+      let match = didYouMean(valueStr, Object.getOwnPropertyNames(myMap));
+      if (match)
+      {
+        const val = myMap[match];
+        for (const key in map)
+        {
+          if (map[key] === val)
+          {
+            match = key;
+            break;
+          }
+        }
+      }
       throw new Error(`Invalid value '${value}' of property '${name}'. ${match !== null ? `Did you mean: '${match}'?` : `Accepted values: ${keys.map(str => `'${str}'`).join(', ')}.`}`);
     }
     option[name] = v;
     return;
   }
 
-  setEnum('module', {
-    None: 0,
-    CommonJS: 1,
-    AMD: 2,
-    UMD: 3,
-    System: 4,
-    ES2015: 5,
-    ESNext: 6
-  });
-  setEnum('moduleResolution', {
-    Classic: 1,
-    NodeJs: 2
-  });
+  setEnum('module', ts.ModuleKind);
+  setEnum('moduleResolution', ts.ModuleResolutionKind);
   setEnum('newLine', {
-    CarriageReturnLineFeed: 0,
-    LineFeed: 1,
-    CrLf: 0,
-    Lf: 1,
-    '\r\n': 0,
-    '\r': 1
+    CrLf: ts.NewLineKind.CarriageReturnLineFeed,
+    Lf: ts.NewLineKind.LineFeed
   });
-  setEnum('target', {
-    ES3: 0,
-    ES5: 1,
-    ES2015: 2,
-    ES2016: 3,
-    ES2017: 4,
-    ES2018: 5,
-    ESNext: 6,
-    JSON: 100,
-    Latest: 6
-  });
-  setEnum('jsx', {
-    None: 0,
-    Preserve: 1,
-    React: 2,
-    ReactNative: 3
-  });
+  setEnum('target', ts.ScriptTarget);
+  setEnum('jsx', ts.JsxEmit);
   return ts.transpile(data.text, option, data.path);
 }
 
