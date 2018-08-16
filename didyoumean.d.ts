3d067c1f178267940312cd4e64b010c28fa456e6
OpportunityLiu <opportunity@live.in>
2018-08-16 12:25:39 +0800

    use ts

---
+++ didyoumean.d.ts
@@ -0,0 +1,17 @@
+﻿interface DidYouMaen
+{
+    threshold: number;
+    caseSensitive: boolean;
+    nullResultValue: string | null;
+    returnWinningObject: boolean;
+    returnFirstMatch: boolean;
+
+    (str: string, list: Array<string>): string | null;
+    <T>(str: string, list: Array<T>, key: keyof T): string | T | null;
+}
+
+declare module 'didyoumean'
+{
+    let didyoumaen: DidYouMaen;
+    export = didyoumaen;
+}
--- index.ts
+++ index.ts
@@ -1,94 +1,91 @@
-const ts = require('typescript');
-const didYouMean = require('didyoumean');
+import * as ts from 'typescript';
+/// <reference path="didyoumean.d.ts"/>
+import didYouMean = require('didyoumean');
 
-/**
- * 
- * @param {any} map 
- * @returns {{[k:string]: number}}
- */
-function getMap(map)
+didYouMean.caseSensitive = false;
+
+interface HexoRendererData
 {
-  let r = {};
-  for (const key in map)
-  {
-    const val = map[key];
-    if (typeof val === 'number')
-      r[key] = val;
-  }
-  return r;
+    text: string;
+    path?: string;
 }
 
-/**
- * 
- * @param {{text:string, path:string}} data 
- * @param {ts.CompilerOptions} options
- */
-function tsRenderer(data, options)
-{
-  /**
-   * @type {ts.CompilerOptions}
-   */
-  var fileOptions = undefined;
-  if (hexo.config && hexo.config.render && typeof hexo.config.render.ts === 'object')
-    fileOptions = hexo.config.render.ts;
-  /**
-   * @type {ts.CompilerOptions}
-   */
-  var option = { ...fileOptions, ...options };
-
-  /**
-   * @param {string} name 
-   * @param {{[k:string]: number}} map
-   */
-  function setEnum(name, map)
-  {
-    map = getMap(map);
-    var value = option[name];
-    if (value === undefined || value === null)
-    {
-      delete option[name];
-      return;
+declare const hexo: {
+    config: { render?: { ts?: ts.CompilerOptions } };
+    extend: {
+        renderer: {
+            register(source: string, target: string, renderer: (data: HexoRendererData, options: any) => string, sync: true): void;
+            register(source: string, target: string, renderer: (data: HexoRendererData, options: any, callback: Function) => void, sync: false): void;
+        }
     }
-    const valueStr = String(value).trim().toLowerCase();
-    const myMap = {};
+};
+
+class NumberMap
+{
+    [key: string]: number | undefined;
+}
+
+function getMap(map: { [k: string]: string | number })
+{
+    let r = new NumberMap();
     for (const key in map)
     {
-      const val = map[key];
-      myMap[key.toLowerCase()] = val;
-      myMap[map[key]] = val;
+        const val = map[key];
+        if (typeof val === 'number')
+            r[key] = val;
     }
-    var v = myMap[valueStr];
-    if (v === undefined)
-    {
-      const keys = Object.getOwnPropertyNames(map);
-      let match = didYouMean(valueStr, Object.getOwnPropertyNames(myMap));
-      if (match)
-      {
-        const val = myMap[match];
-        for (const key in map)
-        {
-          if (map[key] === val)
-          {
-            match = key;
-            break;
-          }
-        }
-      }
-      throw new Error(`Invalid value '${value}' of property '${name}'. ${match !== null ? `Did you mean: '${match}'?` : `Accepted values: ${keys.map(str => `'${str}'`).join(', ')}.`}`);
-    }
-    option[name] = v;
-    return;
-  }
+    return r;
+}
 
-  setEnum('module', ts.ModuleKind);
-  setEnum('moduleResolution', ts.ModuleResolutionKind);
-  setEnum('newLine', {
-    CrLf: ts.NewLineKind.CarriageReturnLineFeed,
-    Lf: ts.NewLineKind.LineFeed
-  });
-  setEnum('target', ts.ScriptTarget);
-  setEnum('jsx', ts.JsxEmit);
-  return ts.transpile(data.text, option, data.path);
+function tsRenderer(data: HexoRendererData, options: ts.CompilerOptions)
+{
+    const fileOptions = hexo && hexo.config && hexo.config.render && hexo.config.render.ts;
+    const option: ts.CompilerOptions = { ...fileOptions, ...options };
+
+    function setEnum(name: keyof ts.CompilerOptions, map: any)
+    {
+        const enumMap = getMap(map);
+        const value = option[name];
+        if (value === undefined || value === null)
+        {
+            delete option[name];
+            return;
+        }
+        const valueStr = String(value).trim().toLowerCase();
+        const myMap = new NumberMap();
+        for (const key in enumMap)
+        {
+            const val = enumMap[key];
+            if (val)
+            {
+                myMap[key.toLowerCase()] = val;
+                myMap[val.toString()] = val;
+            }
+        }
+        var v = myMap[valueStr];
+        if (v === undefined)
+        {
+            const keys = Object.getOwnPropertyNames(enumMap);
+            let match = didYouMean(valueStr, keys);
+            const suggest = match
+                ? `Did you mean: '${match}'?`
+                : `Accepted values: ${keys.map(str => `'${str}'`).join(', ')}.`;
+
+            throw new Error(`Invalid value '${value}' of property '${name}'. ${suggest}`);
+        }
+        option[name] = v;
+        return;
+    }
+
+    setEnum('module', ts.ModuleKind);
+    setEnum('moduleResolution', ts.ModuleResolutionKind);
+    setEnum('newLine', {
+        CrLf: ts.NewLineKind.CarriageReturnLineFeed,
+        Lf: ts.NewLineKind.LineFeed
+    });
+    setEnum('target', ts.ScriptTarget);
+    setEnum('jsx', ts.JsxEmit);
+    return ts.transpile(data.text, option, data.path);
 }
 
 hexo.extend.renderer.register('ts', 'js', tsRenderer, true);
